2039dc87dc47bd6a337d331d2cc9d5fdc5c2b1e2
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-26 19:05:01 +0530

    tupe and enum

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
 "use strict";
+let mySize = 1;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// let user: [number, string, boolean] = [1,"midhun",true]
+// console.log(user)
+// user.push(100)
+// console.log(user)
+
+// class User{
+//     constructor(public id : number, public name : string){
+
+//     }
+// }
+
+// let userOne = new User(1,"midhun")
+// console.log("userOne : ",userOne)
+
+// const enum Size  {small , medium , large}
+// let mySize : Size = Size.medium
+// console.log(mySize)
+
