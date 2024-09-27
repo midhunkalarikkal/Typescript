fff6720d53d7837b4d22966b754a93c71c4e4a30
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 00:36:46 +0530

    annotation and function arguments and return type

--- index.ts
+++ index.ts
@@ -6,7 +6,60 @@
 // heading.textContent = message
 // document.body.appendChild(heading)
 
+// EXAMPLE TWO INTERFACE
+//======================
 
-let num : number = 1
-num = 2
-console.log(num)
+// interface Product{
+//     id : number, 
+//     name : string,
+//     price : number
+// };
+
+// function getProduct(id : number) : Product{
+//     return {
+//         id : id,
+//         name : `Awesome Gadget ${id}`,
+//         price : 99.5
+//     }
+// }
+
+// const showProduct = (name : string, price : number)=>{
+//     console.log(`The product ${name} cost ${price}`);
+// }
+
+// const product = getProduct(1)
+// showProduct(product.name, product.price)
+
+
+// EXAMPLE FOUR
+// ===============
+//Property doesnt exist for the heading, it will show an error
+
+// const heading = document.querySelector('h1')
+// heading?.removeAttribute()
+
+
+// EXAMPLE FIVE OBJECT TYPE ANNOTATION
+// ======================================
+
+// let person : {
+//     id : number;
+//     name : string
+// }
+
+// person = {
+//     id : 1, name : "midhun"
+// }
+
+
+// EXAMPLE SIX FUNCTION ARGUMENT AND RETURN TYPE
+// ================================================
+
+// let greeting : (name : string) => string;
+
+// greeting = function(name : string) {
+//     return `Hi ${name}`
+// }
+
+// console.log(greeting('midhun'))
+
