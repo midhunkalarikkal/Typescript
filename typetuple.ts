1e96bbb05738579e5284b921019483c8d48728eb
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 21:58:19 +0530

    type tuple

---
+++ typetuple.ts
@@ -0,0 +1,14 @@
+//  Type tuple
+// --------------
+
+let skill : [string, number];
+skill = ["programming", 5]
+console.log("skill : ",skill)
+
+let rgb , colour : [number, number, number, number?]
+rgb = [1,2,3,4]
+colour = [1,2,3]
+
+console.log("rgb : ",rgb)
+console.log("colour : ",colour)
+
