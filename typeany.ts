fea8e66fae87e10ba21e9de984c4be5076ec8625
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 22:44:15 +0530

    type any

---
+++ typeany.ts
@@ -0,0 +1,17 @@
+// Type any
+// ------------
+
+let dynamicValues : any;
+dynamicValues = 1
+console.log(dynamicValues)
+
+dynamicValues = "Midhun"
+console.log(dynamicValues)
+
+dynamicValues = {num : 10}
+console.log(dynamicValues)
+
+dynamicValues = [1,2,3]
+console.log(dynamicValues)
+
+
