1d49857ef8b33ac3272c96c102a5f50c2d724671
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 16:22:32 +0530

    type number

---
+++ typenumber.ts
@@ -0,0 +1,34 @@
+// Type number
+// -----------
+
+// if we assign 1.0 it will be 1
+
+// Decimal
+let num : number = 1; 
+console.log("Decimal : ",num)
+
+// Hexadecimal
+let hexnum1 : number = 0x123ABF;
+console.log("Hexadecimal : ",hexnum1)
+let hexnum2 : number = 0X123ABF;
+console.log("Hexadecimal : ",hexnum2)
+
+// Floating
+let flnum : number = 1.22;
+console.log("Floating : ",flnum)
+
+// Bigint
+let big: bigint = 9007199254740991n;
+console.log("Bigint : ", big)
+
+// Octal 
+let octnum1 : number = 0o001227
+console.log("octal : ",octnum1)
+let octnum2 : number = 0O00127
+console.log("octal : ",octnum2)
+
+// binary 
+let bin1 : number = 0b110
+console.log("binary : ",bin1)
+let bin2 : number = 0B001
+console.log("binary : ",bin2)
