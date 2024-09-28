93e8f2938afbcd5719c271bd473019d36214c105
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 16:26:24 +0530

    type string

---
+++ typestring.ts
@@ -0,0 +1,13 @@
+// Sting
+// ---------
+
+let name1 : string = "midhun"
+console.log("name in double quotes : ",name1)
+let name2 : string = 'midhun'
+console.log("name in single quotes : ",name2)
+
+let description : string = `Hi
+Hello dear
+How are you`;
+console.log("multi line : ",description)
+console.log("message  : ",`Hi , ${name1}`)
