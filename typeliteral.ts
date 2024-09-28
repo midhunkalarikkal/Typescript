bf8518b4105302bd964779e0d832925b1cd76b92
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-29 01:54:21 +0530

    type literals

---
+++ typeliteral.ts
@@ -0,0 +1,9 @@
+// Type literal
+// ----------------
+
+let mouseEvent: 'click' | 'dblclick' | 'mouseup' | 'mousedown';
+mouseEvent = 'click'; // valid
+mouseEvent = 'dblclick'; // valid
+mouseEvent = 'mouseup'; // valid
+mouseEvent = 'mousedown'; // valid
+// mouseEvent = 'mouseover'; // compiler error
