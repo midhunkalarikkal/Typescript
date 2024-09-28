78880485a2e5ecd63d2e36d0c553212cc1dc86ce
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-28 20:42:40 +0530

    type array

---
+++ typearray.ts
@@ -0,0 +1,16 @@
+// Type array
+// --------------
+
+let names : string[] = [];
+names[0] = "Midhun";
+names.push("dev")
+
+console.log(names)
+
+// Multiple type arrya
+// ----------------------
+
+let scores : (string | number)[] = [];
+scores = ["coding",10,"dev",9]
+
+console.log(scores)
