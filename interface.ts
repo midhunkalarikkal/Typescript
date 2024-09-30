a8a708a5ffaf624ed4988f5a089ed62e2cf364c2
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-09-30 16:05:16 +0530

    interface

---
+++ interface.ts
@@ -0,0 +1,58 @@
+interface Person {
+    readonly pssn : string;  // modify when only the first object is created
+    fname: string;
+    middlename ?: string;  // optional property
+    lname: string;
+  }
+  
+  function getFullName(person: Person) {
+    return ` pssn is : ${person.pssn} and name is  ${person.fname} ${person.lname}`;
+  }
+  
+  let jony = {
+    pssn : "123",
+    fname: "jony",
+    lname: "doe",
+  };
+  
+  console.log(getFullName(jony));
+  
+
+// To make code more concise
+// function getFullname({fname , lname} : Person){
+//     return `${this.fname} ${this.lname}`
+// }
+
+// Function type
+// -----------------
+interface stringFormat {
+    (str : string , isUpper : boolean) : string
+}
+
+let format : stringFormat;
+
+format = function (str : string , isUpper : boolean) {
+    return isUpper ? str.toLocaleUpperCase() : str.toLocaleLowerCase()
+}
+
+console.log(format("HI",true))
+
+// Class types
+// ---------------
+
+interface json{
+    toJson() : string;
+}
+
+class cp implements json{
+    constructor(private fname : string, private lname : string) {}
+    toJson() : string {
+        return JSON.stringify({
+            fname : this.fname,
+            lname : this.lname
+        })
+    }
+}
+
+const cp1 = new cp("john","doe")
+console.log(cp1.toJson())
